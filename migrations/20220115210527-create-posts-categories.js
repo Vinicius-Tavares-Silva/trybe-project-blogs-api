5dc07c741721a5c20f77d1d3d3d7149d33cc403f
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-01-15 18:24:22 -0300

    Configura sequelize e cria entidades no banco

---
+++ migrations/20220115210527-create-posts-categories.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostsCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+    queryInterface.addColumn(
+      'PostsCategories',
+      'postId',
+      {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+    );
+    queryInterface.addColumn(
+      'PostsCategories',
+      'categoryId',
+      {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+    );
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostsCategories');
+  }
+};
---
+++ models/blogposts.js
@@ -0,0 +1,23 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class BlogPosts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      BlogPosts.belongsTo(models.User, { as: 'user', foreignKey: 'userId' });
+    }
+  }
+  BlogPosts.init({
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'BlogPosts',
+  });
+  return BlogPosts;
+};
