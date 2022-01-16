2af1985779d3f1d86ba068b9bdad1c8dc4e74b8c
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-01-16 17:19:24 -0300

    Requisito 1 - POST /user

--- controllers/users/post.js
+++ controllers/users/post.js
@@ -1,10 +1,16 @@
 const { Users } = require('../../models');
 const authService = require('../../services/auth');
+const usersService = require('../../services/users');
 
 module.exports = async (req, res, next) => {
   const payload = req.body;
-
+  const { displayName, email, password } = payload;
+  const payloadValidate = await usersService(displayName, email, password);
   try {
+    if (payloadValidate) {
+      const { status, message } = payloadValidate;
+      return res.status(status).send({ message });
+    }
     const { password: _password, ...userWithoutPassword } = await Users.create(payload);
     const token = authService.genToken(userWithoutPassword);
     return res.status(201).send({ token });
