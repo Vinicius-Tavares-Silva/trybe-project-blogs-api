f1ff9ab769386ee391282b4be924ee14b39d1154
Vinicius Tavares <vini_tavares10@hotmail.com>
2022-01-16 19:14:50 -0300

    Requisito 9 - GET /post/:id

--- controllers/posts/get.js
+++ controllers/posts/get.js
@@ -1,20 +1,21 @@
-const { BlogPosts, Users } = require('../../models');
+const { BlogPosts, Users, Categories } = require('../../models');
 
+const fields = ['password', 'createdAt', 'updatedAt'];
 module.exports = async (req, res, next) => {
   try {
     const { id } = req.params;
     const object = await BlogPosts.findByPk(
       id,
-      { include: {
-          model: Users,
-          as: 'user',
-          attributes: { exclude: ['password'] },
-      } },
+      {
+        include: [
+        { model: Users, as: 'user', attributes: { exclude: fields } },
+        { model: Categories, as: 'categories', through: { attributes: [] } },
+      ],
+      },
     );
     if (!object) {
-      return res.status(404).send({ message: 'User does not exist' });
+      return res.status(404).send({ message: 'Post does not exist' });
     }
-
     return res.status(200).send(object);
   } catch (err) {
     next(err);
